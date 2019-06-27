a2b08ca48c141c44161d95eacbf65551d201a6be
webermr <matthew.weber@abbvie.com>
2019-06-26 21:21:53 -0500

    Removed duplicates

--- hsHealth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.HomePageView.as_view()),
-    path('metrics/', views.MetricsPageView.as_view()),
-    path('results/', views.ResultsPageView.as_view()),
-]
